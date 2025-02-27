53d9ba6c09ebea9a25283d8912d1f2f4134966cc
WHITEROSE\tuana <sktt1lka@gmail.com>
2025-02-28 05:29:30 +0700

    lost in the sauce

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,48 @@
 // imports
 const axios = require('axios');
 const _ = require('lodash');
+const { ConnectionTCP } = require('node-vmix')
+const { XmlApi } = require('vmix-js-utils')
 
 const config = require('./config.json');
 
 var dataid = 1000;
 var matchInterval;
+var isInvalidMatch = false;
+
+const connection = new ConnectionTCP('localhost')
+
+connection.on('xml', (xmlData) => {
+  const xmlContent = XmlApi.DataParser.parse(xmlData)
+
+  const inputsRawData = XmlApi.Inputs.extractInputsFromXML(xmlContent)
+  const inputs = XmlApi.Inputs.map(inputsRawData)
+
+  console.log(inputs)
+})
+
+connection.on('connect', () => {
+  console.log('Connected')
+
+  connection.send('XML')
+})
+
+const playerPositionToVMixInput = {
+    1: 'Input1', 
+    2: 'Input2',
+    3: 'Input2',
+    4: 'Input2',
+    5: 'Input2',
+    6: 'Input2',
+    7: 'Input2',
+    8: 'Input2',
+    9: 'Input2',
+    10: 'Input10'
+};
+
+var banList = [];
+var pickList = [];
+var commandQueue = [];
 
 const axiosClient = axios.create({
     baseURL: 'https://esportsdata-sg.mobilelegends.com',
@@ -19,11 +56,21 @@ const axiosClient = axios.create({
 });
 
 const beginLiveMatch = async (matchId) => {
+    if (isInvalidMatch) {
+        return;
+    }
+    console.log(`Begin live match ${matchId}`)
     matchInterval = setInterval(() => {
         axiosClient.get(`/battledata?battleid=${matchId}&dataid=${dataid}`).then(response => {
-            sendDataToVMix(response.data)
+            // sendDataToVMix(response.data);
+
+            handleMatchEvent(response.data.data.state, response.data.data);
+
+            console.log(response.data.data.camp_list[0].ban_hero_list)
+            console.log(response.data.data.state)
         }).catch(error => {
-            console.log(error)
+            // console.error(error)
+            isInvalidMatch = true;
         })
     }, 1000)
 }
@@ -38,9 +85,115 @@ const VMixUrlConstructor = (config, data) => {
     const functionString = config.Function;
     const inputString = config.Input;
     const selectedName = config.SelectedName;
-    const valueString = _.get(data, config.Value, '');
+    var valueString = _.get(data, config.Value, '');
+
+    if (functionString === 'AddInput') {
+        valueString = config.Value
+        return `${baseUrl}/?Function=${functionString}&Value=Image|${valueString}`;
+    }
+
+    if (functionString === 'ListAdd') {
+        return `${baseUrl}/?Function=${functionString}&Input=${inputString}&Value=${valueString}`;
+    }
+
+    if (functionString === 'SetImage') {
+        valueString = config.Value
+        console.log(`${baseUrl}/?Function=${functionString}&Input=${inputString}&Value=${valueString}`)
+        return `${baseUrl}/?Function=${functionString}&Input=${inputString}&SelectedName=${selectedName}&Value=${valueString}`;
+    }
     return `${baseUrl}/?Function=${functionString}&Input=${inputString}&SelectedName=${selectedName}&Value=${valueString}`;
 }
+
+const handleMatchEvent = async (event, matchData) => {
+    if (event === 'init') {
+    }
+
+    if (event === 'start') {
+    }
+
+    if (event === 'ban' || event === 'pick') {
+        const camp_list_1 = matchData.camp_list[0];
+        const camp_list_2 = matchData.camp_list[1];
+        
+        // get newly added ban hero
+        const newBanHero = _.difference(camp_list_1.ban_hero_list, banList);
+        if (newBanHero.length > 0) {
+            banList.push(newBanHero[0]);
+            console.log('ban hero', newBanHero[0]);
+        }
+        // get newly added ban hero
+        const newBanHero2 = _.difference(camp_list_2.ban_hero_list, banList);
+        if (newBanHero2.length > 0) {
+            banList.push(newBanHero2[0]);
+            console.log('ban hero', newBanHero2[0]);
+        }
+
+        matchData.camp_list.forEach(camp => {
+            camp.player_list.forEach(async player => {
+                if (player.picking || player.banning) {
+                    const heroId = player.picking ? player.heroid : player.ban_heroid;
+
+                    if (heroId == 0) {
+                        return;
+                    }
+                    
+                    const heroData = await getHeroData(heroId);
+                    const heroName = heroData.hero_name;
+                    const heroImageUrl = heroData.hero_image;
+                    const action = player.picking ? 'Picking' : 'Banning';
+                    // const playerName = player.name;
+                    const playerPos = player.pos;
+
+                    console.log(`Active Player: ${playerName}, Pos: ${playerPos}, Action: ${action}, Hero: ${heroName}`);
+
+                    
+                    updateVMixOverlay(playerPos, playerName, action, heroImageUrl);
+                }
+            });
+        });
+
+    }
+
+    // if (event === 'pick') {
+    // }
+
+    if (event === 'playing') {
+    }
+}
+
+const getHeroData = async (heroid) => {
+    heroid = heroid.toString().padStart(3, '0');
+    const response = await axios.get(`https://mlbb-wiki-api.vercel.app/api/heroes/HE${heroid}`);
+    return response.data;
+};
+
+const updateVMixOverlay = (playerPos, playerName, action, heroImageUrl) => {
+    const vMixInput = playerPositionToVMixInput[playerPos];
+    if (!vMixInput) {
+        console.error(`No vMix input mapped for player position: ${playerPos}`);
+        return;
+    }
+    
+    switch (action) {
+        case 'Picking':
+            sendVMixCommand(`SetImage&Input=${vMixInput}&SelectedName=HeroImage&Value=${heroImageUrl}`);
+            break;
+        case 'Banning':
+            sendVMixCommand(`SetImage&Input=${vMixInput}&SelectedName=HeroImage&Value=${heroImageUrl}`);
+            break;
+        default:
+            console.error(`Unknown action: ${action}`);
+            break;
+    }
+};
+
+const sendVMixCommand = (command) => {
+    axios.get(`http://localhost:8088/api/?Function=${command}`).then((response) => {
+        console.log(`vMix command sent: ${command}`);
+    }).catch((error) => {
+        console.error(`Error sending vMix command: ${command}`, error);
+    });
+};
 
 const sendDataToVMix = async (data) => {
     axios.all(config.pairConfigs.map(item => {
@@ -62,6 +215,6 @@ const getLiveMatch = () => {
     })
 }
 
-getLiveMatch();
+// getLiveMatch();
 
 beginLiveMatch(config.matchId);
